ea7376e29f592332f01939e786e0f8227de6d147
Amel9821 <amalia21198@gmail.com>
2022-11-30 15:14:48 +0700

    memperbaiki coin collision tapi belum solved

--- ARKite/View/ARView/ARViewContainer.swift
+++ ARKite/View/ARView/ARViewContainer.swift
@@ -8,13 +8,32 @@
 import SwiftUI
 import RealityKit
 import ARKit
+import Combine
 
 struct ARViewContainer: UIViewRepresentable {
     @State var anchor: Experience.ARKite
     @State var arView: ARView
+    @State var collisionSubs: [Cancellable] = []
     
     func makeUIView(context: Context) -> ARView {
+        var kiteEntity = anchor.kite!
+        kiteEntity.generateCollisionShapes(recursive: true)
+//        kiteEntity.components[CollisionComponent] = CollisionComponent(
+//            shapes: [.generateBox(size: [1, 1, 1])],
+//            mode: .trigger,
+//          filter: .sensor
+//        )
+        
         arView.scene.anchors.append(anchor)
+        
+//
+//        // subscribe collision event
+//        if let scene = kiteEntity.scene {
+//            collisionSubs.append(scene.subscribe(to: CollisionEvents.Began.self, on: kiteEntity) { event in
+//                print("NABRAKKKK!!!")
+//            })
+//        }
+        
         return arView
         
     }
--- ARKite/ViewModel/ARViewModel.swift
+++ ARKite/ViewModel/ARViewModel.swift
@@ -12,6 +12,11 @@ import Combine
 class ARViewModel: ObservableObject {
     @Published fileprivate var kite: Entity
     @Published fileprivate var obstacle: Entity
+    //    @Published fileprivate var coin1: Entity
+    //    @Published fileprivate var coin2: Entity
+    //    @Published fileprivate var coin3: Entity
+    //    @Published fileprivate var coin4: Entity
+    
     @Published fileprivate var isForward = false
     @Published fileprivate var isRotate = false
     @State fileprivate var distanceBetweenKite = SIMD3<Float>(0,0,0)
@@ -19,12 +24,19 @@ class ARViewModel: ObservableObject {
     let mainAnchor = try! Experience.loadARKite()
     let arView = ARView(frame: .zero)
     fileprivate let initialPosition = SIMD3<Float>(0,0,0)
-    
+//    var subscriptions: [Cancellable] = []
     //Initialize untuk ambil entity
     init(){
         self.kite = mainAnchor.findEntity(named: "kite")!
         self.obstacle = mainAnchor.findEntity(named: "obstacle")!
-        self.randomCoinPosition(kite)
+//        let coin2 = mainAnchor.findEntity(named: "coin2")!
+        
+        //        self.randomCoinPosition(kite)
+//        mainAnchor.actions.coinIsCollided.onAction = coinCollide
+        //        self.coin1 = mainAnchor.findEntity(named: "coin1")!
+        //        self.coin2 = mainAnchor.findEntity(named: "coin2")!
+        //        self.coin3 = mainAnchor.findEntity(named: "coin3")!
+        //        self.coin4 = mainAnchor.findEntity(named: "coin4")!
         
     }
     
@@ -47,7 +59,7 @@ class ARViewModel: ObservableObject {
         
         //Find kite Angle
         if isForward{
-
+            
             //Mencari panjang layang-layang berjalan
             var travelDistance = simd_distance(distanceBetweenKite, kite.position)
             distanceBetweenKite = kite.position
@@ -79,6 +91,20 @@ class ARViewModel: ObservableObject {
         mainAnchor.notifications.kiteStart.post()
         distanceBetweenKite = kite.position
         mainAnchor.actions.kiteStartEnd.onAction = rotateOn
+        
+        //munculin koin
+        mainAnchor.notifications.showCoin.post()
+        
+        //        collidecoin1
+        //        mainAnchor.actions.collisionCoin1.onAction = randomPositionCoin1(_entity:)
+        //        collidecoin2
+        //        mainAnchor.actions.collisionCoin1.onAction = randomPositionCoin2(_entity:)
+        //        collidecoin3
+        //        mainAnchor.actions.collisionCoin1.onAction = randomPositionCoin3(_entity:)
+        //        collidecoin4
+        //        mainAnchor.actions.collisionCoin1.onAction = randomPositionCoin4(_entity:)
+        
+        
     }
     
     //Function untuk mencari angle
@@ -116,6 +142,29 @@ class ARViewModel: ObservableObject {
         rotateRecursion(entity)
     }
     
+    func coinCollide(_ entity: Entity?) {
+        
+        if let coin = entity {
+            let newCoin = mainAnchor.findEntity(named: coin.name)!.clone(recursive: true)
+            let posisiCoinX = newCoin.position.x
+            let posisiCoinY = newCoin.position.y
+            
+//            coin.removeFromParent()
+            let posX1 = posisiCoinX+0.1
+            let posY1 = posisiCoinY+0.1
+            let posZ1 = newCoin.position.z
+           
+            mainAnchor.addChild(newCoin)
+            
+            newCoin.position = SIMD3<Float>(posX1,posY1,posZ1)
+            print(posX1)
+            print(posY1)
+            print(posZ1)
+            
+        }
+    }
+    
+    
     
     
     //Function untuk menyalakan obstacle
@@ -144,24 +193,91 @@ class ARViewModel: ObservableObject {
     
     
     
+    
     //Function untuk mengubah posisi koin setelah collide
-    fileprivate func randomCoinPosition(_ entity: Entity?) {
-        let kite = entity!
-        let posisiLayanganX = kite.position.x
-        let posisiLayanganY = kite.position.y
-        let posisiLayanganZ = kite.position.z
-        let allDisplayAction = mainAnchor.actions.allActions.filter({$0.identifier.hasPrefix("CollisionCoin")})
-        for displayAction in allDisplayAction {
-            displayAction.onAction = {entity in
-                
-                if let entity = entity{
-                    let posX1 = Float.random(in: posisiLayanganX-0.1...posisiLayanganX+0.1)
-                    let posY1 = Float.random(in: posisiLayanganY-0.1...posisiLayanganY+0.1)
-                    let posZ1 = Float.random(in: posisiLayanganZ-0.1...posisiLayanganZ+0.1)
-                    entity.position = SIMD3<Float>(posX1,posY1,posZ1)
-                    print("\(entity.name) has collide")
-                }
-            }
-        }
-    }
+    //    fileprivate func randomCoinPosition(_ entity: Entity?) {
+    //        let kite = entity!
+    ////        let coin = entity!
+    //        let posisiLayanganX = kite.position.x
+    //        let posisiLayanganY = kite.position.y
+    ////        let posisiLayanganZ = kite.position.z
+    //        let allDisplayAction = mainAnchor.actions.allActions.filter({$0.identifier.hasPrefix("CollisionCoin")})
+    //        for displayAction in allDisplayAction {
+    //            displayAction.onAction = {entity in
+    //
+    //                if let entity = entity{
+    //                    let posX1 = Float.random(in: posisiLayanganX-0.1...posisiLayanganX+0.1)
+    //                    let posY1 = Float.random(in: posisiLayanganY-0.1...posisiLayanganY+0.1)
+    //                    let posZ1 = entity.position.z
+    //                    entity.position = SIMD3<Float>(posX1,posY1,posZ1)
+    //                    print("\(kite.position)")
+    //                    print("\(entity.position)")
+    //                }
+    //            }
+    //        }
+    ////    func randomPositionCoin1 (_entity: Entity?) {
+    //////        let coin1 = mainAnchor.findEntity(named: "coin1")!
+    ////        let initialPositionX1 = coin1.position.x
+    ////        let initialPositionY1 = coin1.position.y
+    ////        let posX1 = Float.random(in: initialPositionX1-0.1...initialPositionX1+0.1)
+    ////        let posY1 = Float.random(in: initialPositionY1-0.1...initialPositionY1+0.1)
+    ////        let posZ1 = coin1.position.z
+    ////        coin1.position = SIMD3<Float>(posX1,posY1,posZ1)
+    ////        print("\(coin1.name) has collide")
+    ////    }
+    ////    func randomPositionCoin2 (_entity: Entity?) {
+    //////        let coin2 = mainAnchor.findEntity(named: "coin2")!
+    ////        let initialPositionX2 = coin2.position.x
+    ////        let initialPositionY2 = coin2.position.y
+    ////        let posX2 = Float.random(in: initialPositionX2-0.9...initialPositionX2+0.9)
+    ////        let posY2 = Float.random(in: initialPositionY2-0.9...initialPositionY2+0.9)
+    ////        let posZ2 = coin2.position.z
+    ////        coin2.position = SIMD3<Float>(posX2,posY2,posZ2)
+    ////        print("\(coin2.name) has collide")
+    ////    }
+    ////    func randomPositionCoin3 (_entity: Entity?) {
+    //////        let coin3 = mainAnchor.findEntity(named: "coin3")!
+    ////        let initialPositionX3 = coin3.position.x
+    ////        let initialPositionY3 = coin3.position.y
+    ////
+    ////        let posX3 = Float.random(in: initialPositionX3-0.9...initialPositionX3+0.9)
+    ////        let posY3 = Float.random(in: initialPositionY3-0.9...initialPositionY3+0.9)
+    ////        let posZ3 = coin3.position.z
+    ////        coin3.position = SIMD3<Float>(posX3,posY3,posZ3)
+    ////        print("\(coin3.name) has collide")
+    ////    }
+    ////    func randomPositionCoin4 (_entity: Entity?) {
+    //////        let coin4 = mainAnchor.findEntity(named: "coin4")!
+    ////        let initialPositionX4 = coin4.position.x
+    ////        let initialPositionY4 = coin4.position.y
+    ////
+    ////        let posX4 = Float.random(in: initialPositionX4-0.9...initialPositionX4+0.9)
+    ////        let posY4 = Float.random(in: initialPositionY4-0.9...initialPositionY4+0.9)
+    ////        let posZ4 = coin4.position.z
+    ////        coin4.position = SIMD3<Float>(posX4,posY4,posZ4)
+    ////        print("\(coin4.name) has collide")
+    ////    }
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    ////        var initialPositionZ1 = coin1.position.z
+    //
+    //
+    ////        var initialPositionZ2 = coin2.position.z
+    //
+    //
+    ////        var initialPositionZ3 = coin3.position.z
+    //
+    //
+    ////        var initialPositionZ4 = coin4.position.z
+    //
+    //
+    //
+    //
+    //
+    //    }
 }
