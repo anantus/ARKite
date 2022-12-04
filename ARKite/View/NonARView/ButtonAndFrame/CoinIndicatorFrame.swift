e42bd38f44d5688c4084bae8953e1ea7bd116844
“rickysilitonga112” <rickysilitonga112@gmail.com>
2022-12-04 14:18:02 +0700

    add radius to the coin frame path

--- ARKite/View/NonARView/ButtonAndFrame/CoinIndicatorFrame.swift
+++ ARKite/View/NonARView/ButtonAndFrame/CoinIndicatorFrame.swift
@@ -31,19 +31,28 @@ struct CoinIndicatorPath: Shape {
     func path(in rect: CGRect) -> Path {
         var path = Path()
         //start point - Top left
-        path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.move(to: CGPoint(x: rect.minX, y: rect.minY + 2))
+        
+        // add an arc
+        path.addQuadCurve(to: CGPoint(x: rect.minX + 2, y: rect.minY), control: CGPoint(x: rect.minX, y: rect.minY))
         
         //Move to top right
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY+9))
+        path.addLine(to: CGPoint(x: rect.maxX - 2, y: rect.minY+7))
+        
+        // add an arc
+        path.addQuadCurve(to: CGPoint(x: rect.maxX, y: rect.minY+9), control: CGPoint(x: rect.maxX, y: rect.minY + 7))
         
         //Move to bottom right
-        path.addLine(to: CGPoint(x: rect.maxX-7, y: rect.maxY-4))
+        path.addLine(to: CGPoint(x: rect.maxX - 7 , y: rect.maxY-7))
+        
+        // add an arc
+        path.addQuadCurve(to: CGPoint(x: rect.maxX-8.5, y: rect.maxY-5.5), control: CGPoint(x: rect.maxX - 7.5, y: rect.maxY -  6))
         
         //Move to bottom left
         path.addLine(to: CGPoint(x: rect.minX+4, y: rect.maxY))
         
         //end at top right
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY+2))
         
         return path
     }
